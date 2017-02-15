bd81ca9ccd48542619354b4d016f552faae33a11
Bhavyanth Kolli <bhavyanth.kolli@gmail.com>
2017-02-15 13:31:03 +0530

    Reverted to CustomText component

--- index.ios.js
+++ index.ios.js
@@ -18,10 +18,7 @@ export default class Playground extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image
-        style={styles.image}
-        source={{uri: 'http://www.reactnativeexpress.com/logo.png'}}
-      />
+        <CustomText textContent='Yolo'/>
       </View>
     )
   }
@@ -32,11 +29,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  image: {
-    width: 200,
-    height: 200,
-  },
+  }
 })
 
 AppRegistry.registerComponent('Playground', () => Playground);
